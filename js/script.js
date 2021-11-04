59302fea2ad314a68640081dca4e4034d80b3532
ElenaLj <elenaljubijankic@gmail.com>
2021-11-04 16:26:47 +0100

    init pagina, prove js e primo prompt

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// ************ prove javascript
+
+//alert("Hello World!")
+
+//document.writeln("Hello World")
+
+//document.getElementById("title").innerHTML = "Hello World";
+
+//const myVar = "Hello World!";
+//console.log(myVar)
+
+//let variableOne = "Pippo";
+//let variableTwo = "Pluto";
+
+//variableTwo = "Paperino";
+//console.log(variableTwo)
+
+//esercizio password
+
+const myName = prompt("Inserisci qui il tuo nome");
+console.log(myName);
