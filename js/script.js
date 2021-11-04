4b2782b009e4f75c70537bc54a2b792a8a372da2
ElenaLj <elenaljubijankic@gmail.com>
2021-11-04 16:52:13 +0100

    insert comment js e file css

--- js/script.js
+++ js/script.js
@@ -17,14 +17,22 @@
 
 //esercizio password
 
+// finestra nome
 const myName = prompt("Inserisci qui il tuo nome");
 console.log(myName);
 
+//finestra cognome
 const mySurname = prompt("Inserisci qui il tuo cognome");
 console.log(mySurname);
 
+//finestra colore pref.
 const myFavColor = prompt("Inserisci qui il tuo colore preferito");
 console.log(myFavColor);
 
+
+//generatore password
 let myPassword = myName + mySurname + myFavColor + 21;
 console.log(myPassword);
+
+//output in html
+document.getElementById("password").innerHTML = myPassword;
